81bb4798b869415ca71ecbc97c6c21222030596f
Aliciaegea <aliegea4@gmail.com>
2021-07-27 10:57:29 +0200

    esqueleto proyecto

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 1, name: 'Iron Man' },
+      { id: 2, name: 'The Hulk' },
+      { id: 3, name: 'Spiderman' },
+      { id: 4, name: 'Capitán América' },
+      { id: 5, name: 'Thor' },
+      { id: 6, name: 'Dr. Strange' },
+      { id: 7, name: 'Professor X' },
+      { id: 8, name: 'Storm' },
+      { id: 9, name: 'Luke Cage' },
+      { id: 10, name: 'Black Panter' },
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' },
+    ];
+    return { heroes };
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0
+      ? Math.max(...heroes.map((hero) => hero.id)) + 1
+      : 1;
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Iron Man' },
+  { id: 2, name: 'The Hulk' },
+  { id: 3, name: 'Spiderman' },
+  { id: 4, name: 'Capitán América' },
+  { id: 5, name: 'Thor' },
+  { id: 6, name: 'Dr. Strange' },
+  { id: 7, name: 'Professor X' },
+  { id: 8, name: 'Storm' },
+  { id: 9, name: 'Luke Cage' },
+  { id: 10, name: 'Black Panter' },
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
